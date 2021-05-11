3c19a1b2c0002ae4ec8bd970d5d3b131faa38861
--global <dgarcia@grupomabarak.com.mx>
2021-05-11 15:59:44 -0500

    avance hasta el video de 13

---
+++ src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movi } from 'src/app/models/carterleraResponse';
+
+@Component({
+  selector: 'app-peliculas-poster-grid',
+  templateUrl: './peliculas-poster-grid.component.html',
+  styleUrls: ['./peliculas-poster-grid.component.css']
+})
+export class PeliculasPosterGridComponent implements OnInit {
+  @Input() movies: Movi[];
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.movies);
+  }
+
+}
--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movi } from 'src/app/models/carterleraResponse';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
   @Input() movies: Movi[];
+  swiper: Swiper;
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.movies);
+
   }
 
+  ngAfterViewInit(): void {
+    this.swiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      loop: true,      
+    });
+  }
+  onSlideNext(){
+    this.swiper.slideNext()
+  }
+
+  onSlidePrev(){
+    this.swiper.slidePrev();
+  }
 }
